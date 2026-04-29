faa467aa8020f044648b55cbe5bba68a002fce87
niklasblock <niklas.block.business@gmail.com>
2026-04-29 20:41:34 +0200

    add web UI links table, tag filter and status toggle

--- api/main.py
+++ api/main.py
@@ -3,8 +3,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.database import engine, Base
 from api import models
 from api.routers import links
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
 app.include_router(links.router)
 
 # CORS — damit die Browser Extension zugreifen darf
---
+++ frontend/app.js
@@ -0,0 +1,53 @@
+async function  loadLinks() {
+    try {
+        const tag = document.getElementById("tag-filter").value;
+        const url = tag ? `/links?tag=${tag}` : `/links`;
+        const response = await fetch(url);
+        const data = await response.json(); 
+    
+        console.log(data, response)
+
+        const tbody = document.getElementById("readlater-body"); 
+        tbody.innerHTML=""; 
+        data.forEach(link => {
+            const row = document.createElement("tr"); 
+            const statusBtn = link.read
+                ? `<button onclick="toggleStatus(${link.id})">✓ gelesen</button>`
+                : `<button onclick="toggleStatus(${link.id})">ungelesen</button>`;
+            row.innerHTML = `
+                <td>${link.saved_at}</td> 
+                <td>${link.title}</td>
+                <td>${link.url}</td>
+                <td>${link.tags || "-"}</td>
+                <td>${link.note}</td>
+                <td>${statusBtn}</td>
+                <td><button class="delete-btn" onclick="deleteLink(${link.id})">x</button></td>
+            `; 
+            tbody.appendChild(row); 
+
+        });
+    
+    } catch (error) {
+        console.error('Fehler beim Aufrufen:', error)
+    }
+}
+
+
+async function toggleStatus(id) {
+    await fetch(`/links/${id}/read`, {method: "PATCH"});
+    loadLinks();
+}
+
+async function deleteLink(id) {
+    await fetch(`/links/${id}`, {method: "DELETE"});
+    document.getElementById("readlater-body").innerHTML = "";
+    loadLinks();
+}
+
+// --- Event Listener 
+document.addEventListener("DOMContentLoaded", () => {
+    loadLinks(); 
+    document.getElementById("tag-filter").addEventListener("input", () => {
+        loadLinks();
+    });
+}); 
